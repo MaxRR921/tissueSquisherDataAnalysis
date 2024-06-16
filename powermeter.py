c570e4d738642adc5525d01915d706003d6cd030
Max Richter <marichter@chapman.edu>
2024-06-16 13:26:29 -0700

    reverted bc not working

--- powermeter.py
+++ powermeter.py
@@ -16,29 +16,29 @@ class Powermeter:
             self.OphirCom.StopAllStreams() 
             self.OphirCom.CloseAll()
             # Scan for connected Devices
-            DeviceList = self.OphirCom.ScanUSB()
-            print(DeviceList)
+            self.deviceList = self.OphirCom.ScanUSB()
+            print(self.deviceList)
             # if any device is connected
             
             # thread = Thread(target = self.power.start, args=[])
             # thread.start()
 
 
-            for Device in DeviceList:   
+            for device in self.deviceList:   
             
-                DeviceHandle = self.OphirCom.OpenUSBDevice(Device)# open first device
-                exists = self.OphirCom.IsSensorExists(DeviceHandle, 0)
+                deviceHandle = self.OphirCom.OpenUSBDevice(device)# open first device
+                exists = self.OphirCom.IsSensorExists(deviceHandle, 0)
                 if exists:
-                    print('\n----------Data for S/N {0} ---------------'.format(Device))
+                    print('\n----------Data for S/N {0} ---------------'.format(device))
                     # An Example for data retrieving
-                    self.OphirCom.StartStream(DeviceHandle, 0)# start measuring
+                    self.OphirCom.StartStream(deviceHandle, 0)# start measuring
                     for i in range(10):
                         time.sleep(.2)# wait a little for data
-                        data = self.OphirCom.GetData(DeviceHandle, 0)
+                        data = self.OphirCom.GetData(deviceHandle, 0)
                         if len(data[0]) > 0: # if any data available, print the first one from the batch
                             print('Reading = {0}, TimeStamp = {1}, Status = {2} '.format(data[0][0] ,data[1][0] ,data[2][0]))
                 else:
-                    print('\nNo Sensor attached to {0} !!!'.format(Device))
+                    print('\nNo Sensor attached to {0} !!!'.format(device))
         except OSError as err:
             print("OS error: {0}".format(err))
         except:
