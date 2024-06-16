7a694d0923726cc5fdb1db7c014f782b4147e60f
Max Richter <marichter@chapman.edu>
2024-06-16 13:36:20 -0700

    reverted bc not working

--- powermeter.py
+++ powermeter.py
@@ -17,7 +17,8 @@ class Powermeter:
             self.OphirCom.CloseAll()
             # Scan for connected Devices
             self.deviceList = self.OphirCom.ScanUSB()
-            print(self.deviceList)
+            print(self.deviceList[0])
+            print(self.deviceList[1])
             # if any device is connected
             
             thread = Thread(target = self.__runDevices, args=[])
@@ -42,16 +43,16 @@ class Powermeter:
     def __runDevices(self):
         for device in self.deviceList:   
             
-                deviceHandle = self.OphirCom.OpenUSBDevice(device)# open first device
-                exists = self.OphirCom.IsSensorExists(deviceHandle, 0)
-                if exists:
-                    print('\n----------Data for S/N {0} ---------------'.format(device))
-                    # An Example for data retrieving
-                    self.OphirCom.StartStream(deviceHandle, 0)# start measuring
-                    for i in range(10):
-                        time.sleep(.2)# wait a little for data
-                        data = self.OphirCom.GetData(deviceHandle, 0)
-                        if len(data[0]) > 0: # if any data available, print the first one from the batch
-                            print('Reading = {0}, TimeStamp = {1}, Status = {2} '.format(data[0][0] ,data[1][0] ,data[2][0]))
-                else:
-                    print('\nNo Sensor attached to {0} !!!'.format(device))
+            deviceHandle = self.OphirCom.OpenUSBDevice(device)# open first device
+            exists = self.OphirCom.IsSensorExists(deviceHandle, 0)
+            if exists:
+                print('\n----------Data for S/N {0} ---------------'.format(device))
+                # An Example for data retrieving
+                self.OphirCom.StartStream(deviceHandle, 0)# start measuring
+                for i in range(10):
+                    time.sleep(.2)# wait a little for data
+                    data = self.OphirCom.GetData(deviceHandle, 0)
+                    if len(data[0]) > 0: # if any data available, print the first one from the batch
+                        print('Reading = {0}, TimeStamp = {1}, Status = {2} '.format(data[0][0] ,data[1][0] ,data[2][0]))
+            else:
+                print('\nNo Sensor attached to {0} !!!'.format(device))
