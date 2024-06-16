a0c72ecc561b97ab4f7135583d6244666aee00a4
Max Richter <marichter@chapman.edu>
2024-06-16 12:52:09 -0700

    testing again without threading

--- powermeter.py
+++ powermeter.py
@@ -24,13 +24,30 @@ class Powermeter:
             # if any device is connected
             print("bar")
             # there are two devices
-            power1 = Thread(target = self.__collectData, args=[self.deviceList[0]])
-            power2 = Thread(target= self.__collectData, args=[self.deviceList[1]])
+            # power1 = Thread(target = self.__collectData, args=[self.deviceList[0]])
+            # power2 = Thread(target= self.__collectData, args=[self.deviceList[1]])
 
-            power1.start()
-            power2.start()
-            power1.join()
-            power2.join()
+            # power1.start()
+            # power2.start()
+            # power1.join()
+            # power2.join()
+
+            for device in self.deviceList:
+                deviceHandle = self.OphirCOM.OpenUSBdevice(device)# open first device
+                exists = self.OphirCOM.IsSensorExists(deviceHandle, 0)
+                print("collectdata runs")
+                if exists:
+                    print('\n----------Data for S/N {0} ---------------'.format(device))
+                    # An Example for data retrieving
+                    self.OphirCOM.StartStream(deviceHandle, 0)# start measuring
+                    for i in range(10):
+                        time.sleep(.2)# wait a little for data
+                        data = self.OphirCOM.GetData(deviceHandle, 0)
+                        if len(data[0]) > 0: # if any data available, print the first one from the batch
+                            print('Reading = {0}, TimeStamp = {1}, Status = {2} '.format(data[0][0] ,data[1][0] ,data[2][0]))
+
+                else:
+                    print('\nNo Sensor attached to {0} !!!'.format(device))
             
         except OSError as err:
             print("OS error: {0}".format(err))
