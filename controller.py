90183f25ac8b0dc38b184c28abc43d77d03aae92
Max Richter <marichter@chapman.edu>
2024-06-23 13:54:16 -0700

    h

--- controller.py
+++ controller.py
@@ -87,7 +87,6 @@ class Controller:
         self.ser.write(b'1TS\r\n')
         time.sleep(1)
         print(self.ser.readline())
-        time.sleep(1)
         print(self.ser.readline())
         if(self.ser.readline().strip() == compare):
             print("RUNS")
