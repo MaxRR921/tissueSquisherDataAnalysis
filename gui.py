0346ae20008520aae4f225f2b29aa9593ef627b4
Maxwell Richter <maxwellrichter1@gmail.com>
2025-01-22 17:18:03 -0800

    fixed hopefully

--- gui.py
+++ gui.py
@@ -296,7 +296,7 @@ class Gui:
         listTemp = []
         listTemp.append(loadMove)
         listTemp.append(unloadMove)
-        self.saveNumExecutions("3")
+        self.saveNumExecutions(tk.StringVar("3"))
         self.startExecuteThread(listTemp)
         self.alphaVals.append(self.powerPlot.maxValY - self.powerPlot.minValY)
         alpha_vals_temp_label = ttk.Label(frame, text="alpha values: " + str(self.alphaVals))
