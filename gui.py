a821e9c1e768fabbe8966043aef4abaab6a27918
Maxwell Richter <maxwellrichter1@gmail.com>
2025-01-31 18:16:31 -0800

    fixed finally

--- gui.py
+++ gui.py
@@ -243,6 +243,7 @@ class Gui:
     def __alignAlpha(self):
         if self.powerPlot is None:
             self.powerPlot = Plot2D('power plot', 'distance (mm)', 'power (um)', True)
+            self.plotList.append(self.powerPlot)
         
         self.alphaVals = []
         # Create a new pop-up window
