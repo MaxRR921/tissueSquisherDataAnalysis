64e3cdcc3b18d228cc3789eec68499be6b9ce9d9
Maxwell Richter <maxwellrichter1@gmail.com>
2025-03-22 15:16:57 -0700

    fix

--- graphingProcess.py
+++ graphingProcess.py
@@ -106,7 +106,7 @@ class GraphingProcess(QtWidgets.QMainWindow):
         
         interp_func = interp1d(self.x_data3, self.y_data3, kind='linear', fill_value='extrapolate')
         aligned_pow2 = interp_func(self.x_data2)
-        diff = self.y_data1 - aligned_pow2
+        diff = self.y_data2 - aligned_pow2
 
         
         self.curve1.setData(self.x_data1, self.y_data1)
