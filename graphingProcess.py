8bba07c2113cd12b52860319aceb621918bfd5b7
Maxwell Richter <maxwellrichter1@gmail.com>
2025-04-07 15:47:00 -0700

    added broken part

--- graphingProcess.py
+++ graphingProcess.py
@@ -132,18 +132,18 @@ class GraphingProcess(QtWidgets.QMainWindow):
             while not self.strainQueue.empty():
                 self.x_data4.append(self.strainQueue.get_nowait())
         
-        # if  self.x_data3 and self.y_data3 and self.y_data2:
-        #     interp_func = interp1d(self.x_data3, self.y_data3, kind='linear', fill_value='extrapolate')
-        #     aligned_pow2 = interp_func(self.x_data2)
-        #     diff = self.y_data2 - aligned_pow2
+        if  self.x_data3 and self.y_data3 and self.y_data2:
+            interp_func = interp1d(self.x_data3, self.y_data3, kind='linear', fill_value='extrapolate')
+            aligned_pow2 = interp_func(self.x_data2)
+            diff = self.y_data2 - aligned_pow2
 
         
         self.curve1.setData(self.x_data1, self.y_data1)
         self.curve2.setData(self.x_data2, self.y_data2)
         self.curve3.setData(self.x_data3, self.y_data3)
         self.curve4.setData(self.x_data4, self.y_data4)
-        # if diff is not None:
-        #     self.curve5.setData(self.x_data2, diff)
+        if diff is not None:
+            self.curve5.setData(self.x_data2, diff)
 
 
 
