4432e4f4178e84e4a838b52d0962aa17300a1f10
Maxwell Richter <maxwellrichter1@gmail.com>
2025-04-23 17:41:52 -0700

    use this branch for calibration code rn

--- calibration.py
+++ calibration.py
@@ -352,50 +352,80 @@ def ex():
 #         ax.set_zlabel('Ex')
 
 #         plt.show()
+
+class Calibration:
+     def __init__(self, npoints, force_values):
+          # Material properties of the fiber
+          self.Y = 7.3e10 # young's modulus of the fiber in N/m^2
+          self.sigma = 0.17 # Poisson's ratio at 633nm wavelength
+          self.N = 1.46 # Refractive index of the fiber 
+          self.p_11 = 0.121 #photelastic constants 
+          self.p_12 = 0.27
+          self.b = 62.5e-6 # Radius of fiber cladding in meters 
+          self.Lb_0 = 2e-3 # unstressed beat length in meters
+
+
+          self.powermeter_area = 0.0071 # powermeter sensor area in meters 
+
+          #calculated values
+          self.S1 = np.zeros(npoints) # powermeter 1 power
+          self.S2 = np.zeros(npoints) # powermeter 2 power 
+          self.Sdifference = np.zeros(npoints) # difference between the two powers 
+          self.ExList = np.zeros(npoints, dtype=complex)
+          self.EyList = np.zeros(npoints, dtype=complex)
+
+
+
+
+
+          
 def ex1(alphaVal):
-        npoints = 500
-        # Material properties of fiber
-        Y = 7.3e10  # Young's modulus of fiber in N/m^2
-        sigma = 0.17  # Poisson's ratio at 633nm wavelength
-        N = 1.46  # Refractive index
-        N_s = 1.7
-        N_f = 2.0
-        N_s = float(N_s)
-        N_f = float(N_f)
-        p_11 = 0.121  # Photoelastic constants
-        p_12 = 0.27
-        p_44 = (p_11 - p_12) / 2  # For isotropic materials
-        b = 62.5e-6  # Radius of cladding in meters
-        Lb_0 = 2e-3  # Unstressed beat length, 2 mm
+     npoints = 500
+     # Material properties of fiber
+     Y = 7.3e10  # Young's modulus of fiber in N/m^2
+     sigma = 0.17  # Poisson's ratio at 633nm wavelength
+     N = 1.46  # Refractive index
+     N_s = 1.7
+     N_f = 2.0
+     N_s = float(N_s)
+     N_f = float(N_f)
+     p_11 = 0.121  # Photoelastic constants
+     p_12 = 0.27
+     p_44 = (p_11 - p_12) / 2  # For isotropic materials
+     b = 62.5e-6  # Radius of cladding in meters
+     Lb_0 = 2e-3  # Unstressed beat length, 2 mm
 
-        S1 = np.zeros(npoints, dtype=complex)
-        S2 = np.zeros(npoints, dtype=complex)
-        Sdifference = np.zeros(npoints, dtype=complex)
-        ExList = np.zeros(npoints, dtype=complex)
-        EyList = np.zeros(npoints, dtype=complex)
+     area = .0071 
 
-        f = np.linspace(0, 90, npoints)  # Force in N/m (2D analysis)
-        l = np.linspace(7.25, 20, npoints)  # Interaction length, about 18 mm
-        Ex_0 = 1
-        lambda_light = 1550e-9  # Wavelength of light in fiber
-        k = 1/lambda_light
+     S1 = np.zeros(npoints, dtype=complex)
+     S2 = np.zeros(npoints, dtype=complex)
+     Sdifference = np.zeros(npoints, dtype=complex)
+     ExList = np.zeros(npoints, dtype=complex)
+     EyList = np.zeros(npoints, dtype=complex)
 
-        alpha = alphaVal # angle between applied force and fast and slow axis of the fiber.
-        beta = np.pi / 6  # Angle between polarized light and fast and slow axis of the fiber.
-        delta = 1  # Extra phase from traveling through unstressed fiber
-        gamma = np.pi/2# Angle of PM fiber wrt polarimeter. Should be 0 or pi/2
-        eta = 376.730313 
+     f = np.linspace(0, 100, npoints)  # Force in N/m (2D analysis)
+     l = np.linspace(7.25, 20, npoints)  # Interaction length, about 18 mm
+     Ex_0 = 1
+     lambda_light = 1550e-9  # Wavelength of light in fiber
+     k = 1/lambda_light
 
-        F = 2 * N**3 * (1 + sigma) * (p_12 - p_11) * Lb_0 * f / (lambda_light * np.pi * b * Y)  # Normalized force66
-        phiVals = 0.5 * np.arctan((F * np.sin(2 * alpha)) / (1 + F * np.cos(2 * alpha)))  # Angle of rotated birefringence
-        Lb = Lb_0 * (1 + F**2 + 2 * F * np.cos(2 * alpha))**(-1/2)  # Modified beat length
+     alpha = alphaVal # angle between applied force and fast and slow axis of the fiber.
+     beta = np.pi / 6  # Angle between polarized light and fast and slow axis of the fiber.
+     delta = 1  # Extra phase from traveling through unstressed fiber
+     gamma = np.pi/2# Angle of PM fiber wrt polarimeter. Should be 0 or pi/2
+     eta = 376.730313 
+
+     F = 2 * N**3 * (1 + sigma) * (p_12 - p_11) * Lb_0 * f / (lambda_light * np.pi * b * Y)  # Normalized force66
+     phiVals = 0.5 * np.arctan((F * np.sin(2 * alpha)) / (1 + F * np.cos(2 * alpha)))  # Angle of rotated birefringence
+     #    phiVals = np.linspace(0,, npoints)
+     Lb = Lb_0 * (1 + F**2 + 2 * F * np.cos(2 * alpha))**(-1/2)  # Modified beat length
      #    print("PhiVals:", phiVals)
      #    print("LbVals: ", Lb)
      #    print("F", F)
-        initialPowerDifference = 0
-        finalPowerDifference = 0
-        for li in range(npoints):
-        
+     initialPowerDifference = 0
+     finalPowerDifference = 0
+     for li in range(npoints):
+     
                
                A = ((np.cos(gamma)*np.cos(phiVals[li])) + (np.exp(1j*delta) * np.sin(gamma) * np.sin(phiVals[li])))
                B = ((-np.cos(gamma) * np.sin(phiVals[li])) + (np.exp(1j*delta) * np.sin(gamma) * np.cos(phiVals[li])))
@@ -429,52 +459,55 @@ def ex1(alphaVal):
                # print("Ey2: ", Ey2)
                eta = 376.73  # Characteristic impedance of free space (ohms)
 
-               S1[li] = Ex2/(2*eta)
-               S2[li] = Ey2/(2*eta)
+               S1[li] = Ex2/(2*eta) * area
+               S2[li] = Ey2/(2*eta) * area
+               
+               # S1[li] = np.real(S1[li])
+               # S2[li] = np.real(S2[li])
 
                
                Sdifference[li] = S2[li] - S1[li]
                
                if (li == 0):
-                      initialPowerDifference = Sdifference[li]
-                      print("INIT: ", initialPowerDifference)
+                    initialPowerDifference = Sdifference[li]
+                    print("INIT: ", initialPowerDifference)
 
                if(li == npoints - 1):
-                      finalPowerDifference = Sdifference[li]
-                      print("FIN: ", finalPowerDifference)
+                    finalPowerDifference = Sdifference[li]
+                    print("FIN: ", finalPowerDifference)
 
                ExList[li] = Ex
                EyList[li] = Ey
 
 
-     #    print("S1: ", S1) 
-     #    print("S2: ", S2)
-     #    print("Sdifference: ", Sdifference)
+     print("S1: ", S1) 
+     print("S2: ", S2)
+     print("Sdifference: ", Sdifference)
      #    print(ExList)
      #    print(EyList) 
-     #    plt.figure()
-     #    plt.plot(f, S1, label='S1')
-     #    plt.plot(f, S2, label='S2')
-     #    plt.xlabel('Force (N/m)')
-     #    plt.ylabel('S1, S2 (Real part)')
-     #    plt.title('S1 and S2 vs. Force')
-     #    plt.legend()
-     #    plt.grid(True)
-     #    plt.show()
+     plt.figure()
+     plt.plot(f, S1, label='S1')
+     plt.plot(f, S2, label='S2')
+     plt.xlabel('Force (N/m)')
+     plt.ylabel('S1, S2 (Real part)')
+     plt.title('S1 and S2 vs. Force')
+     plt.legend()
+     plt.grid(True)
+     plt.show()
 
 
-       # Second Figure: Plot just the difference
-     #    plt.figure()
-     #    plt.plot(f, Sdifference, label='Difference (S2 - S1)', color='red')
-     #    plt.xlabel('Force (N/m)')
-     #    plt.ylabel('Difference (Real part)')
-     #    plt.title('Difference (S1 - S2) vs. Force')
-     #    plt.legend()
-     #    plt.grid(True)
+     # Second Figure: Plot just the difference
+     plt.figure()
+     plt.plot(f, Sdifference, label='Difference (S2 - S1)', color='red')
+     plt.xlabel('Force (N/m)')
+     plt.ylabel('Difference (Real part)')
+     plt.title('Difference (S1 - S2) vs. Force')
+     plt.legend()
+     plt.grid(True)
 
-     #    plt.show()
-     #    print("VALUE: ", finalPowerDifference - initialPowerDifference)
-        return (finalPowerDifference-initialPowerDifference)
+     plt.show()
+     print("VALUE: ", finalPowerDifference - initialPowerDifference)
+     return (finalPowerDifference-initialPowerDifference)
         
 
 
@@ -538,8 +571,8 @@ def test_ex1():
 
             
             
-test_ex1()
-# ex1(np.pi/6)
+# test_ex1()
+ex1(np.pi/6)
 
 
 
